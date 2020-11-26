7f6662d3d8ea295efd55bd03c4de1539080b69bd
GoG_Paulie <ppolih21@gmail.com>
2020-11-25 21:00:59 -0500

    gallery work

--- src/App.js
+++ src/App.js
@@ -1,74 +1,19 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 import '../src/index.css';
-import { Button, IconButton } from '@material-ui/core';
-import ButtonGroup from '@material-ui/core/ButtonGroup';
-import Typography from '@material-ui/core/Typography';
-import SaveIcon from '@material-ui/icons/Save';
-import HighlightOffSharpIcon from '@material-ui/icons/HighlightOffSharp';
-import Checkbox from '@material-ui/core/Checkbox';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import TextField from '@material-ui/core/TextField';
-import Navbar from './components/Navbar';
-import Projects from './components/Projects';
 
-const CheckboxExample = () => {
-  const [checked, setChecked] = useState(true);
-  return (
-    <FormControlLabel
-      control={
-        <Checkbox
-          onChange={(e) => setChecked(e.target.checked)}
-          checked={checked}
-          icon={<HighlightOffSharpIcon color='secondary' />}
-          checkedIcon={<SaveIcon />}
-          color='primary'
-          imputProps={{
-            'aria-label': 'secondary checkbox',
-          }}
-        />
-      }
-      label='Testing Checkbox'
-    />
-  );
-};
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Projects from './components/Projects';
 
 function App() {
   return (
     <div className='App'>
       <Navbar />
-      <div className='hero'>
-        <Typography variant='h2' component='h2'>
-          Vanessa Brasfield
-        </Typography>
-        <Typography variant='h4' component='h4'>
-          Graphic Design
-        </Typography>
-      </div>
+      <Hero />
       <Projects id='projects' />
-      <TextField style={{ color: 'white' }} variant='filled' label='Name' />
-      <CheckboxExample />
-      <ButtonGroup>
-        <Button
-          size='large'
-          variant='contained'
-          color='primary'
-          startIcon={<SaveIcon />}
-        >
-          Save
-        </Button>
-        {/* <IconButton aria-label='delete' color='secondary'>
-          <TapAndPlayIcon />
-        </IconButton> */}
-        <Button
-          size='large'
-          variant='contained'
-          color='secondary'
-          startIcon={<HighlightOffSharpIcon />}
-        >
-          Discard
-        </Button>
-      </ButtonGroup>
+      {/* <TextField style={{ color: 'white' }} variant='filled' label='Name' /> */}
+      {/* <CheckboxExample /> */}
     </div>
   );
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,14 +8,14 @@ const Projects = () => {
   });
 
   return (
-    <div id='projects'>
-      <h1>Freelance Projects</h1>
-      <p>
+    <div id='projects' className='projects'>
+      <h1 className='projects__header'>Freelance Projects</h1>
+      <p className='projects__info'>
         Contract freelance work with a focus on Twitch, digital marketing and
         business identity. In my free time I do traditional life drawing and
         freehand sketching.
       </p>
-      <p>
+      <p className='projects__info'>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet
         praesentium ex veniam soluta molestiae vitae est. Dolores animi in rem
         consequuntur autem architecto nihil ratione error? Et impedit ut
@@ -28,31 +28,34 @@ const Projects = () => {
         nulla?Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet
         praesentium ex veniam soluta molestiae vitae est. Dolores animi in rem
       </p>
-      <h2>Avatars</h2>
-      <Grid container justify='center'>
+      <h2 className='projects__category'>Avatars</h2>
+      <Grid container className='gallery-grid' justify='center'>
         {imageData.map((image) => {
           if (image.category === 'avatar') {
             return (
-              <Grid item key={image.id} xs={4}>
+              <Grid item key={image.id} xs={3} spacing={2}>
                 <img
-                  className='gallery-image gallery-image--avatar'
+                  className='gallery-grid__img--avatar'
                   src={image.src}
+                  alt={image.category}
                 />
               </Grid>
             );
           }
+          return null;
         })}
       </Grid>
-      <h2>Panels</h2>
+      <h2 className='projects__category'>Panels</h2>
       <Grid container spacing={1}>
         {imageData.map((image) => {
           if (image.category === 'panel') {
             return (
-              <Grid item key={image.id} item xs={6} sm={3}>
-                <img src={image.src} />
+              <Grid item key={image.id} xs={3} sm={3}>
+                <img src={image.src} alt={image.category} />
               </Grid>
             );
           }
+          return null;
         })}
       </Grid>
     </div>
